c8b0465df08d940457aa43bcee6d5a7b305d990f
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-22 14:41:44 +0900

    Update putting_out_package_box.cpp

--- putting_out_package_box.cpp
+++ putting_out_package_box.cpp
@@ -1,29 +1,27 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-vector<int> store;
-
 int solution(int n, int w, int num) {
-    int answer = 0;
-    
-    for(int i = 1; i<w*2; i+=2){
-        store.push_back(i);
+   
+    int row = (num - 1) / w;
+
+    int col;
+    if (row % 2 == 0) {                 
+        col = (num - 1) % w;
+    } else {                            
+        col = w - 1 - ((num - 1) % w);
     }
-    
-    sort(store.rbegin(), store.rend());
-    
-    int floor = num/w - (num%w==0?1:0);
-    
-    int nextNum=num;
-    while(nextNum<=n){
-        int addNum;
-        addNum = nextNum%w==0?w:nextNum%w;
-        nextNum = nextNum+store[addNum-1];
-        answer++;
-        floor++;
+
+    int total_boxes = 0;
+
+   
+    for (int r = row; r <= (n - 1) / w; ++r) {
+        int box_num;
+        if (r % 2 == 0) {               
+            box_num = r * w + (col + 1);
+        } else {                        
+            box_num = r * w + (w - col);
+        }
+        if (box_num > n) break;        
+        ++total_boxes;
     }
-    
-    return answer;
+
+    return total_boxes;
 }
