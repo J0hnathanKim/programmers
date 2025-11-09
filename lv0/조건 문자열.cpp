6f3b7544dc9aca07483c9f5f3310b7cee213f9ee
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-11-09 22:47:44 +0900

    Add solution function for inequality evaluation
    Implement solution function to evaluate inequalities

---
+++ lv0/조건 문자열.cpp
@@ -0,0 +1,21 @@
+#include <string>
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+
+    if (ineq == ">" && eq == "=") {
+        answer = (n >= m);
+    }
+    else if (ineq == ">" && eq == "!") {
+        answer = (n > m);
+    }
+    else if (ineq == "<" && eq == "=") {
+        answer = (n <= m);
+    }
+    else if (ineq == "<" && eq == "!") {
+        answer = (n < m);
+    }
+
+    return answer;
+}
